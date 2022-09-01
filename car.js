2016bafe5c441dca8bc1cfe5696d416497400a16
Omar <omarkiojoo@gmail.com>
2022-09-01 17:53:19 +0100

    sensor

--- car.js
+++ car.js
@@ -6,17 +6,20 @@ class Car {
     this.height = height
 
     this.speed = 0
-    this.acceleration = 0.08
+    this.acceleration = 0.2
     this.maxSpeed = 20
-    this.friction = 0.04
+    this.friction = 0.05
     this.angle = 0
 
+    this.sensor = new Sensor(this)
     this.controls = new Controls()
   }
 
-  update() {
+  update(roadBorders) {
     this.#move()
+    this.sensor.update(roadBorders)
   }
+
   #move() {
     if (this.controls.forward) {
       this.speed += this.acceleration
@@ -44,14 +47,14 @@ class Car {
 
     if (this.speed != 0) {
       const flip = this.speed > 0 ? 1 : -1
-
       if (this.controls.left) {
-        this.angle += 0.0075 * flip
+        this.angle += 0.02 * flip
       }
       if (this.controls.right) {
-        this.angle -= 0.0075 * flip
+        this.angle -= 0.02 * flip
       }
     }
+
     this.x -= Math.sin(this.angle) * this.speed
     this.y -= Math.cos(this.angle) * this.speed
   }
@@ -59,11 +62,14 @@ class Car {
   draw(ctx) {
     ctx.save()
     ctx.translate(this.x, this.y)
-    ctx.rotate(this.angle)
+    ctx.rotate(-this.angle)
 
     ctx.beginPath()
     ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height)
     ctx.fill()
+
     ctx.restore()
+
+    this.sensor.draw(ctx)
   }
 }
