3427d94ef77b3f88c74577d0ba83aec97a44b000
Omar <omarkiojoo@gmail.com>
2022-09-03 19:39:16 +0100

    neural network ??

--- car.js
+++ car.js
@@ -1,39 +1,85 @@
 class Car {
-  constructor(x, y, width, height) {
+  constructor(
+    x,
+    y,
+    width,
+    height,
+    maxSpeed = 20,
+    controlType,
+    color = "black"
+  ) {
     this.x = x
     this.y = y
     this.width = width
     this.height = height
 
     this.speed = 0
-    this.acceleration = 0.1
-    this.maxSpeed = 15
-    this.friction = 0.05
+    this.acceleration = 0.125
+    this.maxSpeed = maxSpeed
+    this.friction = 0.03
     this.angle = 0
-    this.dameged = false
+    this.damaged = false
 
-    this.sensor = new Sensor(this)
-    this.controls = new Controls()
+    this.useBrain = controlType == "AI"
+
+    if (controlType !== "Dummy") {
+      this.sensor = new Sensor(this)
+      this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4])
+    }
+    this.controls = new Controls(controlType)
 
     this.img = new Image()
     this.img.src = "./assets/car.png"
+
+    this.mask = document.createElement("canvas")
+    this.mask.width = width
+    this.mask.height = height
+
+    const maskCtx = this.mask.getContext("2d")
+    this.img.onload = () => {
+      maskCtx.fillStyle = color
+      maskCtx.rect(0, 0, this.width, this.height)
+      maskCtx.fill()
+
+      maskCtx.globalCompositeOperation = "destination-atop"
+      maskCtx.drawImage(this.img, 0, 0, this.width, this.height)
+    }
   }
 
-  update(roadBorders) {
+  update(roadBorders, traffic) {
     if (!this.damaged) {
       this.#move()
       this.polygon = this.#createPolygon()
-      this.damaged = this.#assessDamage(roadBorders)
+      this.damaged = this.#assessDamage(roadBorders, traffic)
     }
-    this.sensor.update(roadBorders)
+    if (this.sensor) {
+      this.sensor.update(roadBorders, traffic)
+      const offsets = this.sensor.readings.map((s) =>
+        s == null ? 0 : 1 - s.offsets
+      )
+      const outputs = NeuralNetwork.feedForward(offsets, this.brain)
+
+      if (this.useBrain) {
+        this.controls.forward = outputs[0]
+        this.controls.left = outputs[1]
+        this.controls.right = outputs[2]
+        this.controls.reverse = outputs[3]
+      }
+    }
   }
 
-  #assessDamage(roadBorders) {
+  #assessDamage(roadBorders, traffic) {
     for (let i = 0; i < roadBorders.length; i++) {
       if (polysIntersect(this.polygon, roadBorders[i])) {
         return true
       }
     }
+    for (let i = 0; i < traffic.length; i++) {
+      if (polysIntersect(this.polygon, traffic[i].polygon)) {
+        return true
+      }
+    }
+
     return false
   }
   #createPolygon() {
@@ -98,30 +144,45 @@ class Car {
     this.y -= Math.cos(this.angle) * this.speed
   }
 
-  draw(ctx) {
-    // if (this.damaged) {
-    //   ctx.fillStyle = "lightgray"
-    // } else {
-    //   ctx.fillStyle = "black"
-    // }
-    // ctx.beginPath()
-    // ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
-    // for (let i = 1; i < this.polygon.length; i++) {
-    //   ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
-    // }
-    // ctx.fill()
-    ctx.save()
-    ctx.translate(this.x, this.y)
-    ctx.rotate(-this.angle)
-    ctx.drawImage(
-      this.img,
-      -this.width / 2,
-      -this.height / 2,
-      this.width,
-      this.height
-    )
-    ctx.restore()
+  // draw(ctx) {
+  //   ctx.save()
+  //   ctx.translate(this.x, this.y)
+  //   ctx.rotate(-this.angle)
+  //   // ctx.drawImage(
+  //   //   this.mask,
+  //   //   -this.width / 2,
+  //   //   -this.height / 2,
+  //   //   this.width,
+  //   //   this.height
+  //   // )
+  //   // ctx.globalCompositeOperation = "multiply"
+  //   ctx.drawImage(
+  //     this.img,
+  //     -this.width / 2,
+  //     -this.height / 2,
+  //     this.width,
+  //     this.height
+  //   )
+  //   ctx.restore()
+  //   if (this.sensor) {
+  //     this.sensor.draw(ctx)
+  //   }
+  // }
+  draw(ctx, color) {
+    if (this.damaged) {
+      ctx.fillStyle = "gray"
+    } else {
+      ctx.fillStyle = color
+    }
+    ctx.beginPath()
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
+    for (let i = 1; i < this.polygon.length; i++) {
+      ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
+    }
+    ctx.fill()
 
-    this.sensor.draw(ctx)
+    if (this.sensor) {
+      this.sensor.draw(ctx)
+    }
   }
 }
--- main.js
+++ main.js
@@ -3,12 +3,18 @@ canvas.width = 400
 
 const ctx = canvas.getContext("2d")
 const road = new Road(canvas.width / 2, canvas.width * 0.9)
-const car = new Car(road.getLaneCenter(1), 100, 55, 125)
+const car = new Car(road.getLaneCenter(1), 100, 60, 130, 10, "AI")
+//
+const traffic = [new Car(road.getLaneCenter(1), -100, 60, 130, 8, "Dummy")]
 
 animate()
 
 function animate() {
-  car.update(road.borders)
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders, [])
+  }
+
+  car.update(road.borders, traffic)
 
   canvas.height = window.innerHeight
 
@@ -16,7 +22,10 @@ function animate() {
   ctx.translate(0, -car.y + canvas.height * 0.7)
 
   road.draw(ctx)
-  car.draw(ctx)
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(ctx, "blue")
+  }
+  car.draw(ctx, "red")
 
   ctx.restore()
   requestAnimationFrame(animate)
